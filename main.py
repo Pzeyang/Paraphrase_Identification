af0206b51538665006b97d499a3695c84a3238e3
Robert Fischer <robert@programming-with-design.at>
2016-04-16 12:57:26 +0300

    added semantic similarity feature began with cardinal number similarity

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from __future__ import division
-import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.translate.bleu_score import sentence_bleu
-from nltk.util import ngrams
 from nltk.util import skipgrams
+from nltk.corpus import wordnet
 import math
-import numpy
 from sklearn import neighbors
 from sklearn import svm
 from sklearn import linear_model
+from treetagger import TreeTagger
+from itertools import product
+import numpy as np
+import text2int as t2i
 
 def combinations(n, k):
     f = math.factorial
@@ -79,6 +81,56 @@ def computeSentenceSimilarityFeatures(sentence1, sentence2):
 
     return features
 
+
+# Uses treetagger-python (Installation https://github.com/miotto/treetagger-python ; http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/)
+def computeSemanticSimilarityFeatures(sentence1, sentence2):
+    features = [0] * 2
+
+    tt = TreeTagger(language='english')
+    tags1 = tt.tag(sentence1)
+    tags2 = tt.tag(sentence2)
+
+    # Feature: noun/web semantic similarity
+
+    # Get Synonym set
+    def synSet(tags):
+        for word in tags:
+            # Only compare Nouns or Verbs
+            if word[1][0] != 'N' and word[1][:2] != 'VV':
+                continue
+
+            word.append(wordnet.synsets(word[2]))
+
+    synSet(tags=tags1)
+    synSet(tags=tags2)
+
+    sims = []
+
+    # TODO: Maybe do not use product() instead use max() similarity
+    for word1, word2 in product(tags1, tags2):
+        type1 = word1[1]
+        type2 = word2[1]
+
+        if type1[0] != 'N' and type1[:2] != 'VV' or type1 != type2:
+            continue
+
+        similarity = 0
+        for sense1, sense2 in product(word1[3], word2[3]):
+            similarity = max(similarity, wordnet.wup_similarity(sense1, sense2))
+
+        sims.append(similarity)
+
+
+    features[0] = np.sum(sims) / len(sims)
+
+    # Feature: Cardinal number similarity
+    for word1 in tags1:
+        if word1[1] == 'CD':
+            pass
+
+    return features
+
+
 # Read and process train and test data
 def readData():
     trainFeat = []
@@ -91,7 +143,8 @@ def readData():
     for i in range(0,4076):
         tokens = f.readline().strip().split('\t')
         trainClass[i] = int(tokens[0])
-        trainFeat.append(computeSentenceSimilarityFeatures(tokens[3], tokens[4]))
+        trainFeat.append(computeSentenceSimilarityFeatures(tokens[3], tokens[4])
+                         .extend(computeSemanticSimilarityFeatures(tokens[3], tokens[4])))
     f.close()
 
     f = open("msr_paraphrase_test.txt", "r")
@@ -99,7 +152,8 @@ def readData():
     for i in range(0,1725):
         tokens = f.readline().strip().split('\t')
         testClass[i] = int(tokens[0])
-        testFeat.append(computeSentenceSimilarityFeatures(tokens[3], tokens[4]))
+        testFeat.append(computeSentenceSimilarityFeatures(tokens[3], tokens[4])
+                        .extend(computeSemanticSimilarityFeatures(tokens[3], tokens[4])))
     f.close()
 
     return trainFeat, trainClass, testFeat, testClass
